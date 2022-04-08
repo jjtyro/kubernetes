68d6833201156e8ba19ff5cd627cf3708a4172c5
jj.tyro <jj.tyro@gmail.com>
2022-04-08 11:47:12 +0800

    continue disable kernel memory account.

--- vendor/github.com/opencontainers/runc/libcontainer/cgroups/systemd/apply_systemd.go
+++ vendor/github.com/opencontainers/runc/libcontainer/cgroups/systemd/apply_systemd.go
@@ -5,7 +5,6 @@ package systemd
 import (
 	"errors"
 	"fmt"
-	"github.com/golang/glog"
 	"os"
 	"path/filepath"
 	"strings"
@@ -542,8 +541,6 @@ func getUnitName(c *configs.Cgroup) string {
 
 func setKernelMemory(c *configs.Cgroup) error {
 	path, err := getSubsystemPath(c, "memory")
-	glog.Warningf("apply_systemd.go setKernelMemory: %v.", path)
-
 	if err != nil && !cgroups.IsNotFound(err) {
 		return err
 	}
